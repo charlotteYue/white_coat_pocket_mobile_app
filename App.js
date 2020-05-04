c26aaf1f120e811c489e55e253e09b6a96fc57cc
lymmm412 <lymmm412@163.com>
2020-05-04 16:07:07 -0400

    get category name from db

--- App.js
+++ App.js
@@ -25,39 +25,39 @@ const Stack = createStackNavigator();
 export default class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      currentUserId: undefined,
-      client: undefined,
-    };
-    this._loadClient = this._loadClient.bind(this);
-    this._onPressLogin = this._onPressLogin.bind(this);
-    this._onPressLogout = this._onPressLogout.bind(this);
+    // this.state = {
+    //   currentUserId: undefined,
+    //   client: undefined,
+    // };
+    // this._loadClient = this._loadClient.bind(this);
+    // this._onPressLogin = this._onPressLogin.bind(this);
+    // this._onPressLogout = this._onPressLogout.bind(this);
   }
 
 
-  async componentDidMount() {
-   await this._loadClient();
+  componentDidMount() {
+  //  this._loadClient();
   }
 
   render() {
-    let loginStatus = 'Currently logged out.';
+    // let loginStatus = 'Currently logged out.';
     
-    if (this.state.currentUserId) {
-      loginStatus = `Currently logged in as ${this.state.currentUserId}!`;}
+    // if (this.state.currentUserId) {
+    //   loginStatus = `Currently logged in as ${this.state.currentUserId}!`;}
 
-    loginButton = (
-      <Button
-        onPress={this._onPressLogin}
-        title="Login"
-      />
-    );
+    // loginButton = (
+    //   <Button
+    //     onPress={this._onPressLogin}
+    //     title="Login"
+    //   />
+    // );
 
-    logoutButton = (
-      <Button
-        onPress={this._onPressLogout}
-        title="Logout"
-      />
-    );
+    // logoutButton = (
+    //   <Button
+    //     onPress={this._onPressLogout}
+    //     title="Logout"
+    //   />
+    // );
     return (
       <>
         <NavigationContainer>
@@ -97,59 +97,59 @@ export default class App extends React.Component {
     );
   }
 
-  async _loadClient() {
-    console.log('here in app.js');
-    await Stitch.initializeDefaultAppClient(
-      'wcpg-bxtzi'
-    ).then(client => {
-      this.setState({ client });
-      if (client.auth.isLoggedIn) {
-        this.setState({
-          currentUserId: client.auth.user.id,
-        });
-      }
-    });
-  }
+  // async _loadClient() {
+  //   console.log('here in app.js');
+  //   await Stitch.initializeDefaultAppClient(
+  //     'wcpg-bxtzi'
+  //   ).then(client => {
+  //     this.setState({ client });
+  //     if (client.auth.isLoggedIn) {
+  //       this.setState({
+  //         currentUserId: client.auth.user.id,
+  //       });
+  //     }
+  //   });
+  // }
 
-  _onPressLogin() {
-    this.state.client.auth
-      .loginWithCredential(
-        new AnonymousCredential()
-      )
-      .then(user => {
-        console.log(
-          `Successfully logged in as user ${
-            user.id
-          }`
-        );
-        this.setState({ currentUserId: user.id });
-      })
-      .catch(err => {
-        console.log(
-          `Failed to log in anonymously: ${err}`
-        );
-        this.setState({
-          currentUserId: undefined,
-        });
-      });
-  }
+  // _onPressLogin() {
+  //   this.state.client.auth
+  //     .loginWithCredential(
+  //       new AnonymousCredential()
+  //     )
+  //     .then(user => {
+  //       console.log(
+  //         `Successfully logged in as user ${
+  //           user.id
+  //         }`
+  //       );
+  //       this.setState({ currentUserId: user.id });
+  //     })
+  //     .catch(err => {
+  //       console.log(
+  //         `Failed to log in anonymously: ${err}`
+  //       );
+  //       this.setState({
+  //         currentUserId: undefined,
+  //       });
+  //     });
+  // }
 
-  _onPressLogout() {
-    this.state.client.auth
-      .logout()
-      .then(user => {
-        console.log(`Successfully logged out`);
-        this.setState({
-          currentUserId: undefined,
-        });
-      })
-      .catch(err => {
-        console.log(`Failed to log out: ${err}`);
-        this.setState({
-          currentUserId: undefined,
-        });
-      });
-  }
+  // _onPressLogout() {
+  //   this.state.client.auth
+  //     .logout()
+  //     .then(user => {
+  //       console.log(`Successfully logged out`);
+  //       this.setState({
+  //         currentUserId: undefined,
+  //       });
+  //     })
+  //     .catch(err => {
+  //       console.log(`Failed to log out: ${err}`);
+  //       this.setState({
+  //         currentUserId: undefined,
+  //       });
+  //     });
+  // }
 }
 
 const styles = StyleSheet.create({
--- pages/clientHome.js
+++ pages/clientHome.js
@@ -12,26 +12,129 @@ import { ScrollView } from 'react-native-gesture-handler';
 import HeaderComponent from './components/HeaderComponent.js';
 import ButtonGridComponent from './components/ButtonGridComponent.js';
 import FooterComponent from './components/FooterComponent.js';
-// import AdminHome from './pages/adminHome';
-
- class ClientHome extends React.Component {
+import { Stitch, AnonymousCredential, RemoteMongoClient } from 'mongodb-stitch-react-native-sdk';
+ 
+class ClientHome extends React.Component {
    constructor(props){
      super(props);
-     this.state={isAdmin:false};
+     this.state={
+      currentUserId: undefined,
+      client: undefined,
+      data: [],
+     };
+     this._loadClient = this._loadClient.bind(this);
+    // this._onPressLogin = this._onPressLogin.bind(this);
+    // this._onPressLogout = this._onPressLogout.bind(this);
+    this._getCategories=this._getCategories.bind(this)
    }
    
+   async componentDidMount(){
+    await this._loadClient();
+    this._getCategories();
+  }
+
+  // async _loadClient() {
+  //   console.log('here in app.js');
+  //   await Stitch.initializeDefaultAppClient(
+  //     'wcpg-bxtzi'
+  //   ).then(client => {
+  //     this.setState({ client });
+  //     if (client.auth.isLoggedIn) {
+  //       this.setState({
+  //         currentUserId: client.auth.user.id,
+  //       });
+  //     }
+  //   });
+  // }
+
+  // _onPressLogin() {
+  //   this.state.client.auth
+  //     .loginWithCredential(
+  //       new AnonymousCredential()
+  //     )
+  //     .then(user => {
+  //       console.log(
+  //         `Successfully logged in as user ${
+  //           user.id
+  //         }`
+  //       );
+  //       this.setState({ currentUserId: user.id });
+  //     })
+  //     .catch(err => {
+  //       console.log(
+  //         `Failed to log in anonymously: ${err}`
+  //       );
+  //       this.setState({
+  //         currentUserId: undefined,
+  //       });
+  //     });
+  // }
+
+  async _loadClient() {
+    console.log('here in app.js');
+    await Stitch.initializeDefaultAppClient(
+      'wcpg-bxtzi'
+    ).then(client => {
+      this.setState({ client });
+      if (client.auth.isLoggedIn) {
+        this.setState({
+          currentUserId: client.auth.user.id,
+        });
+      }
+    });
+  }
+
+  _getCategories= () => {
+    console.log("here in client")
+    const stitchAppClient = Stitch.defaultAppClient;
+    // console.log('the category chosen is', this.props.categoryName);
+    const mongoClient = stitchAppClient.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas")
+    // const query = {'type': 'Housing' };
+    // const projection = {"projection": {
+    //   "type": 1,
+    //   "_id": 0,
+    // },};
+    stitchAppClient.auth
+        .loginWithCredential(new AnonymousCredential())
+        .then(() => { 
+          const conn = mongoClient.db('test')
+          const db = conn.collection('providers')
+          db.aggregate(
+            [{"$group": { "_id": "$type" }}]
+          )
+          .toArray()
+          .then( res => {
+            // console.log('---------------------');
+            // console.log(res[0]);
+            let arr=new Array();
+            let obj=new Object();
+            for(var i=0;i<res.length;i++){
+              let obj=new Object();
+              obj["name"]=res[i]._id;
+              arr.push(obj);
+            }
+            // console.log(arr);
+            this.setState({data:arr}, function() {
+                console.log(this.state.data);
+              })
+            // if(arr.length===res.length){
+            //   this.setState({data:arr}, function() {
+            //     // console.log(this.state.data);
+            //   })
+            // }
+          }).catch(err => console.error(`Failed to group aggregation: ${err}`))
+
+        }).catch(console.error)
+
+  }
+
    render() {
-    const items = [
-      {name: 'Healthcare Services'},
-      {name: 'Specialty Healthcare'},
-      {name: 'Behavioral Health'},
-      // {name: 'Behavioral Health (Cont.)'},
-      {name: 'Interpersonal Violence'},
-      {name: 'Immigration'},
-      {name: 'Housing'},
-      {name: 'LGBTQ+ Resources'},
-      {name: 'Lifestyle'},
-    ];
+    const items = this.state.data;
+    console.log(items);
+    let loginStatus = 'Currently logged out.';
+    
+    if (this.state.currentUserId) {
+      loginStatus = `Currently logged in as ${this.state.currentUserId}!`;}
 
     return (
       <>
