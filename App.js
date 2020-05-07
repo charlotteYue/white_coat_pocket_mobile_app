4507b1605fbddd5d4029ee60e7be28947c0d603e
Yuqi Chai <yuqi_chai@brown.edu>
2020-05-07 00:02:48 -0400

    try to merge

--- App.js
+++ App.js
@@ -20,44 +20,8 @@ const Stack = createStackNavigator();
 export default class App extends React.Component {
   constructor() {
     super();
-<<<<<<< HEAD
-    // this.state = {
-    //   currentUserId: undefined,
-    //   client: undefined,
-    // };
-    // this._loadClient = this._loadClient.bind(this);
-    // this._onPressLogin = this._onPressLogin.bind(this);
-    // this._onPressLogout = this._onPressLogout.bind(this);
-  }
-
-
-  componentDidMount() {
-  //  this._loadClient();
-  }
-
-  render() {
-    // let loginStatus = 'Currently logged out.';
-    
-    // if (this.state.currentUserId) {
-    //   loginStatus = `Currently logged in as ${this.state.currentUserId}!`;}
-
-    // loginButton = (
-    //   <Button
-    //     onPress={this._onPressLogin}
-    //     title="Login"
-    //   />
-    // );
-
-    // logoutButton = (
-    //   <Button
-    //     onPress={this._onPressLogout}
-    //     title="Logout"
-    //   />
-    // );
-=======
   }
   render() {
->>>>>>> blue-version
     return (
       <>
         <NavigationContainer>
@@ -104,61 +68,4 @@ export default class App extends React.Component {
       </>
     );
   }
-<<<<<<< HEAD
-
-  // async _loadClient() {
-  //   console.log('here in app.js');
-  //   await Stitch.initializeDefaultAppClient(
-  //     'wcpg-bxtzi'
-  //   ).then(client => {
-  //     this.setState({ client });
-  //     if (client.auth.isLoggedIn) {
-  //       this.setState({
-  //         currentUserId: client.auth.user.id,
-  //       });
-  //     }
-  //   });
-  // }
-
-  // _onPressLogin() {
-  //   this.state.client.auth
-  //     .loginWithCredential(
-  //       new AnonymousCredential()
-  //     )
-  //     .then(user => {
-  //       console.log(
-  //         `Successfully logged in as user ${
-  //           user.id
-  //         }`
-  //       );
-  //       this.setState({ currentUserId: user.id });
-  //     })
-  //     .catch(err => {
-  //       console.log(
-  //         `Failed to log in anonymously: ${err}`
-  //       );
-  //       this.setState({
-  //         currentUserId: undefined,
-  //       });
-  //     });
-  // }
-
-  // _onPressLogout() {
-  //   this.state.client.auth
-  //     .logout()
-  //     .then(user => {
-  //       console.log(`Successfully logged out`);
-  //       this.setState({
-  //         currentUserId: undefined,
-  //       });
-  //     })
-  //     .catch(err => {
-  //       console.log(`Failed to log out: ${err}`);
-  //       this.setState({
-  //         currentUserId: undefined,
-  //       });
-  //     });
-  // }
-=======
->>>>>>> blue-version
 }
--- pages/clientHome.js
+++ pages/clientHome.js
@@ -13,24 +13,6 @@ import HeaderComponent from './components/HeaderComponent.js';
 import ButtonGridComponent from './components/ButtonGridComponent.js';
 import FooterComponent from './components/FooterComponent.js';
 import { Stitch, AnonymousCredential, RemoteMongoClient } from 'mongodb-stitch-react-native-sdk';
-<<<<<<< HEAD
- 
-class ClientHome extends React.Component {
-   constructor(props){
-     super(props);
-     this.state={
-      currentUserId: undefined,
-      client: undefined,
-      data: [],
-     };
-     this._loadClient = this._loadClient.bind(this);
-    // this._onPressLogin = this._onPressLogin.bind(this);
-    // this._onPressLogout = this._onPressLogout.bind(this);
-    this._getCategories=this._getCategories.bind(this)
-   }
-   
-   async componentDidMount(){
-=======
 
  class ClientHome extends React.Component {
    constructor(props) {
@@ -46,54 +28,11 @@ class ClientHome extends React.Component {
    }
 
    async componentDidMount() {
->>>>>>> blue-version
     await this._loadClient();
     this._getCategories();
   }
 
-<<<<<<< HEAD
-  // async _loadClient() {
-  //   console.log('here in app.js');
-  //   await Stitch.initializeDefaultAppClient(
-  //     'wcpg-bxtzi'
-  //   ).then(client => {
-  //     this.setState({ client });
-  //     if (client.auth.isLoggedIn) {
-  //       this.setState({
-  //         currentUserId: client.auth.user.id,
-  //       });
-  //     }
-  //   });
-  // }
-
-  // _onPressLogin() {
-  //   this.state.client.auth
-  //     .loginWithCredential(
-  //       new AnonymousCredential()
-  //     )
-  //     .then(user => {
-  //       console.log(
-  //         `Successfully logged in as user ${
-  //           user.id
-  //         }`
-  //       );
-  //       this.setState({ currentUserId: user.id });
-  //     })
-  //     .catch(err => {
-  //       console.log(
-  //         `Failed to log in anonymously: ${err}`
-  //       );
-  //       this.setState({
-  //         currentUserId: undefined,
-  //       });
-  //     });
-  // }
-
   async _loadClient() {
-    console.log('here in app.js');
-=======
-  async _loadClient() {
->>>>>>> blue-version
     await Stitch.initializeDefaultAppClient(
       'wcpg-bxtzi'
     ).then(client => {
@@ -107,48 +46,6 @@ class ClientHome extends React.Component {
   }
 
   _getCategories= () => {
-<<<<<<< HEAD
-    console.log("here in client")
-    const stitchAppClient = Stitch.defaultAppClient;
-    // console.log('the category chosen is', this.props.categoryName);
-    const mongoClient = stitchAppClient.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas")
-    // const query = {'type': 'Housing' };
-    // const projection = {"projection": {
-    //   "type": 1,
-    //   "_id": 0,
-    // },};
-    stitchAppClient.auth
-        .loginWithCredential(new AnonymousCredential())
-        .then(() => { 
-          const conn = mongoClient.db('test')
-          const db = conn.collection('providers')
-          db.aggregate(
-            [{"$group": { "_id": "$type" }}]
-          )
-          .toArray()
-          .then( res => {
-            // console.log('---------------------');
-            // console.log(res[0]);
-            let arr=new Array();
-            let obj=new Object();
-            for(var i=0;i<res.length;i++){
-              let obj=new Object();
-              obj["name"]=res[i]._id;
-              arr.push(obj);
-            }
-            // console.log(arr);
-            this.setState({data:arr}, function() {
-                console.log(this.state.data);
-              })
-            // if(arr.length===res.length){
-            //   this.setState({data:arr}, function() {
-            //     // console.log(this.state.data);
-            //   })
-            // }
-          }).catch(err => console.error(`Failed to group aggregation: ${err}`))
-
-        }).catch(console.error)
-=======
     const stitchAppClient = Stitch.defaultAppClient;
     const mongoClient = stitchAppClient.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas');
 
@@ -178,20 +75,11 @@ class ClientHome extends React.Component {
           }).catch(() => Alert.alert('no content for your selection'));
 
         }).catch(console.error);
->>>>>>> blue-version
 
   }
 
    render() {
     const items = this.state.data;
-<<<<<<< HEAD
-    console.log(items);
-    let loginStatus = 'Currently logged out.';
-    
-    if (this.state.currentUserId) {
-      loginStatus = `Currently logged in as ${this.state.currentUserId}!`;}
-=======
->>>>>>> blue-version
 
     return (
       <>
